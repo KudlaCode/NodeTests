276a9cb36eaee5c3249240a438f152f0e697115e
KudlaCode <unbekanntespferd@hotmail.de>
2022-01-11 00:48:49 +0100

    added file system io

--- build/index.js
+++ build/index.js
@@ -5,8 +5,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const logger_1 = __importDefault(require("./utilities/logger"));
+const fs_1 = require("fs");
+const csvtojson_1 = __importDefault(require("csvtojson"));
 const app = (0, express_1.default)();
 const port = 3000;
+const inputFile = './users.csv';
+const outputFile = 'users.json';
+app.get('/convert', logger_1.default, (req, res) => {
+    res.send('converting in progress!');
+    (0, csvtojson_1.default)()
+        .fromFile(inputFile)
+        .then((data) => {
+        let newData = data.map((item) => {
+            let first = item.first_name;
+            let last = item.last_name;
+            let phone = item.phone;
+            if (item.phone === "") {
+                phone = "missing data";
+            }
+            return { first, last, phone };
+        });
+        fs_1.promises.writeFile(outputFile, JSON.
+            stringify(newData));
+    });
+});
 //routes
 app.get('/continents', logger_1.default, (req, res) => {
     res.send('continents!');
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,33 @@
 import express from 'express';
 import logger from './utilities/logger';
+import { promises as fsPromises } from 'fs';
+import csv from 'csvtojson';
 
 const app = express();
 const port = 3000;
+
+const inputFile = './users.csv';
+const outputFile = 'users.json';
+
+app.get('/convert', logger, (req, res) => {
+  res.send('converting in progress!');
+  csv()
+    .fromFile(inputFile)
+    .then((data: any) => {
+      let newData = data.map(
+        (item: { first_name: string; last_name: string; phone: string }) => {
+          let first = item.first_name;
+          let last = item.last_name;
+          let phone = item.phone;
+          if (item.phone === '') {
+            phone = 'missing data';
+          }
+          return { first, last, phone };
+        }
+      );
+      fsPromises.writeFile(outputFile, JSON.stringify(newData));
+    });
+});
 
 //routes
 app.get('/continents', logger, (req, res) => {
