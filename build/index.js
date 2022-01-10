e0a0be6b1961ff78cbe7c456160ab08097e06219
KudlaCode <unbekanntespferd@hotmail.de>
2022-01-11 00:14:43 +0100

    added routing and logging middleware

--- build/index.js
+++ build/index.js
@@ -4,11 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const logger_1 = __importDefault(require("./utilities/logger"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.get('/api', (req, res) => {
-    res.send('Hello, world!');
+//routes
+app.get('/continents', logger_1.default, (req, res) => {
+    res.send('continents!');
 });
+app.get('/countries', logger_1.default, (req, res) => {
+    res.send('countries!');
+});
+app.get('/oceans', (req, res) => {
+    res.send('oceans!');
+});
+// start the Express server
 app.listen(port, () => {
-    console.log(`server started at localhost:${port}`);
+    console.log(`server started at http://localhost:${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import express from 'express';
+import logger from './utilities/logger';
 
 const app = express();
 const port = 3000;
 
-app.get('/api', (req, res) => {
-  res.send('Hello, world!');
+//routes
+app.get('/continents', logger, (req, res) => {
+  res.send('continents!');
 });
 
+app.get('/countries', logger, (req, res) => {
+  res.send('countries!');
+});
+
+app.get('/oceans', (req, res) => {
+  res.send('oceans!');
+});
+
+// start the Express server
 app.listen(port, () => {
-  console.log(`server started at localhost:${port}`);
+  console.log(`server started at http://localhost:${port}`);
 });
