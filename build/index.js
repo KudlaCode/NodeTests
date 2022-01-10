8ed436163349d0a32291de1db0d35197c78a4c6c
KudlaCode <unbekanntespferd@hotmail.de>
2022-01-10 20:59:44 +0100

    added asynchronous tests using axios api

--- build/index.js
+++ build/index.js
@@ -1,19 +1,71 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const arrays_1 = __importDefault(require("./utilities/arrays"));
-const numbers_1 = __importDefault(require("./utilities/numbers"));
-const strings_1 = __importDefault(require("./utilities/strings"));
-const numArr = [3, 4, 5, 6];
-const wordArr = ['cat', 'dog', 'rabbit', 'bird'];
-const arrSum = arrays_1.default.addArr(numArr);
-const mixArr = arrays_1.default.concatArr(numArr, wordArr);
-const myNum = '15' % 2;
-const five = parseInt('5');
-console.log(arrays_1.default.cut3(mixArr));
-console.log(numbers_1.default.sum(arrSum, myNum));
-console.log(strings_1.default.capitalize('the quick brown fox'));
-console.log(numbers_1.default.multiply(five, 8));
-console.log(arrays_1.default.lgNum(mixArr));
+/** Use Axios to get data from restcountries api */
+const axios_1 = __importDefault(require("axios"));
+/** Use the free API https://restcountries.eu/
+ * You will use the following endpoints
+ * https://restcountries.eu/rest/v2/name/{name} for countries by name
+ * https://restcountries.eu/rest/v2/regionalbloc/{regionalbloc} for region blocks
+ */
+/** Create getCountry Function here */
+function getCountry(country) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            //console.log('before webApi call');
+            const getApi = yield (0, axios_1.default)(`https://restcountries.com/v2/name/${country}`);
+            const data = getApi.data[0];
+            // console.log('finished webApi call. weg got:');
+            // console.log(getApi.data);
+            return {
+                capital: data.capital,
+                region: data.region,
+                numericCode: data.numericCode
+            };
+        }
+        catch (e) {
+            console.error(`error: ${JSON.stringify(e)}`);
+            throw e; // rethrow error or return null, if the request failed
+        }
+    });
+}
+/** Create a test for this getRegion function */
+function getRegionCountries(regionalbloc) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const getApi = yield (0, axios_1.default)(`https://restcountries.com/v2/regionalbloc/${regionalbloc}`);
+        const data = getApi.data;
+        const countries = [];
+        for (let i = 0; i < data.length; i++) {
+            countries.push(data[i].name);
+        }
+        return countries;
+    });
+}
+/** Create getRegionCapitals function here */
+function getRegionCapitals(region) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const getApi = yield (0, axios_1.default)(`https://restcountries.com/v2/regionalbloc/${region}`);
+        const data = getApi.data;
+        const capitals = [];
+        for (let i = 0; i < data.length; i++) {
+            capitals.push(data[i].capital);
+        }
+        return capitals;
+    });
+}
+exports.default = {
+    getCountry,
+    getRegionCountries,
+    getRegionCapitals
+};
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,62 @@
-import arrays from './utilities/arrays';
-import numbers from './utilities/numbers';
-import strings from './utilities/strings';
+/** Use Axios to get data from restcountries api */
+import axios from 'axios';
 
-const numArr = [3, 4, 5, 6];
-const wordArr = ['cat', 'dog', 'rabbit', 'bird'];
-const arrSum = arrays.addArr(numArr);
-const mixArr = arrays.concatArr(numArr, wordArr);
-const myNum = (('15' as unknown) as number) % 2;
-const five = parseInt('5');
+/** Use the free API https://restcountries.eu/
+ * You will use the following endpoints
+ * https://restcountries.eu/rest/v2/name/{name} for countries by name
+ * https://restcountries.eu/rest/v2/regionalbloc/{regionalbloc} for region blocks
+ */
 
-console.log(arrays.cut3(mixArr));
-console.log(numbers.sum(arrSum, myNum));
-console.log(strings.capitalize('the quick brown fox'));
-console.log(numbers.multiply(five, 8));
-console.log(arrays.lgNum(mixArr));
+/** Create getCountry Function here */
+async function getCountry(country: string): Promise<any> {
+  try {
+    //console.log('before webApi call');
+
+    const getApi = await axios(`https://restcountries.com/v2/name/${country}`);
+    const data = getApi.data[0];
+
+    // console.log('finished webApi call. weg got:');
+    // console.log(getApi.data);
+
+    return {
+      capital: data.capital,
+      region: data.region,
+      numericCode: data.numericCode
+    };
+  } catch (e) {
+    console.error(`error: ${JSON.stringify(e)}`);
+    throw e; // rethrow error or return null, if the request failed
+  }
+}
+
+/** Create a test for this getRegion function */
+async function getRegionCountries(regionalbloc: string): Promise<string[]> {
+  const getApi = await axios(
+    `https://restcountries.com/v2/regionalbloc/${regionalbloc}`
+  );
+  const data = getApi.data;
+  const countries = [];
+  for (let i = 0; i < data.length; i++) {
+    countries.push(data[i].name);
+  }
+  return countries;
+}
+
+/** Create getRegionCapitals function here */
+async function getRegionCapitals(region: string): Promise<string[]> {
+  const getApi = await axios(
+    `https://restcountries.com/v2/regionalbloc/${region}`
+  );
+  const data = getApi.data;
+  const capitals = [];
+  for (let i = 0; i < data.length; i++) {
+    capitals.push(data[i].capital);
+  }
+  return capitals;
+}
+
+export default {
+  getCountry,
+  getRegionCountries,
+  getRegionCapitals
+};
