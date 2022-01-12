72045dec70f2c11fefdf18297f36b2d807b201ae
KudlaCode <unbekanntespferd@hotmail.de>
2022-01-12 22:29:43 +0100

    added file caching

--- src/utilities/resize.ts
+++ src/utilities/resize.ts
@@ -1,11 +1,16 @@
 import sharp from 'sharp'
 import path from 'path'
+import fs from 'fs'
 
 const resize = async (filename: string, width: number, height: number) : Promise<string> => {
     const absFilename = path.join(__dirname,'./../../assets/full/' + filename + '.jpg');
     const thumbPath = path.join(__dirname,'./../../assets/thumbs/' + filename + '_' + width + '_' + height  +'.jpg');
-    console.log('input filename: ' + absFilename);
-    await sharp(absFilename).resize(width, height).toFile(thumbPath);
+
+    //only create file if it does not exist
+    if(!fs.existsSync(thumbPath)){
+        console.log('resizing input filename: ' + absFilename + " to " + width + "x" + height);
+        await sharp(absFilename).resize(width, height).toFile(thumbPath);
+    }
     return thumbPath;
   };
 
